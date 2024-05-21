56155d07acb4b6e2602ba2e4ddc7be3074c9e23b
Muhammad Sameem <sameemgujjar123@yahoo.com>
2024-05-21 14:42:31 +0400

    :heavy_check_mark: 5 lessons completed

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_green.jpg",
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
+    };
+  },
+});
